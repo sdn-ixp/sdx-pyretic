c8c1b3baee5504edc63aedc972d1f2faece0d026
Arpit <glex.qsd@gmail.com>
2013-12-17 11:11:39 -0500

    Managing older conflict for main

--- main.py
+++ main.py
@@ -12,7 +12,6 @@
 #        Arpit Gupta (glex.qsd@gmail.com)
 #        Muhammad Shahbaz
 #        Laurent Vanbever
-#        Sean Donovan
 #
 #  Copyright notice:
 #        Copyright (C) 2012, 2013 Georgia Institute of Technology
@@ -39,11 +38,11 @@
 from pyretic.lib.corelib import *
 from pyretic.lib.std import *
 
+from pyretic.modules.arp import *
 from pyretic.modules.mac_learner import *
 
 ## SDX-specific imports
 from pyretic.sdx.lib.core import *
-from pyretic.sdx.utils.arp import *
 import pyretic.sdx.QuaggaInterface.quagga_interface as qI
 
 ## General imports
@@ -109,7 +108,6 @@ class SDX_Policies(DynamicPolicy):
         print "Done with compose"
         return self.policy
     
-<<<<<<< HEAD
     
     def update_policy(self,init):
         self.policy=self.compose_policies (init)
@@ -130,37 +128,5 @@ class SDX_Policies(DynamicPolicy):
 def main():
     
     p=SDX_Policies()
-    return if_(ARP, arp(), if_(BGP, identity, p)) >> mac_learner()
-=======
-    ####
-    #### Apply policies from each participant
-    ####
-    print "Parsing Participant's policies ..."
-    sdx_parse_policies(cwd + '/pyretic/sdx/sdx_policies.cfg', base, participants)
-    #print base
-
-    return ({IPAddr('172.0.0.172'): EthAddr('08:00:27:8b:e4:7b')},sdx_platform(base),base)
-    # {} is the list of local IP-to-MAC addresses used with VNH. Will be extracted from the sdx_platform()
-
-### Main ###
-def main():
-    """Handle ARPs, BGPs, SDX and then do MAC learning"""
-    print "Parsing Configurations"
-    
-    start_parse=time.time()
-    (ip_mac_list,sdx_policy,sdx_base) = sdx()
-    
-    print  time.time() - start_parse, "seconds"
-    #print sdx_policy
-    #for participant in sdx_base.participants:
-    #    print participant.id_
-    print "Compiled SDX Policies"
-    start_comp=time.time()
-    sdx_policy.compile()
-    print  'Aggregate Compilation',time.time() - start_comp, "seconds"
-    
-    # Start the Quagga Interface
-    thread.start_new_thread(qI.main(sdx_base))
-    
-    return if_(ARP, arp(ip_mac_list), if_(BGP, identity, sdx_policy)) >> mac_learner()
->>>>>>> d2acf8bfce366ce778dc89c703ebba3c7a79e7a9
+    ip_mac_list={IPAddr('172.0.0.172'): EthAddr('08:00:27:8b:e4:7b')}
+    return if_(ARP, arp(ip_mac_list), if_(BGP, identity, p)) >> mac_learner()
