1d36c64fafa30bf8f93af48c62f9c59cd16c75f8
Sean Donovan <sdonovan@gatech.edu>
2013-12-19 15:42:03 -0500

    Adding trivial implmentation of Best Path Selection

--- bgp/peer.py
+++ bgp/peer.py
@@ -10,13 +10,13 @@ class peer():
     def __init__(self,ip):
         
         self.ip = ip
-        
-        self.rib = rib()
+        self.input_rib = rib()
+        self.local_rib = rib()
          
     def add_route(self,route):
         
         route = json.loads(route)
-        
+
         origin = None
         as_path = None
         med = None
@@ -24,7 +24,8 @@ class peer():
         
         if ('neighbor' in route):
             if ('ip' in route['neighbor']):
-                if (route['neighbor']['ip'] == self.ip):
+                # Only add to the RIB if it's from a participant who is not ourselves.
+                if (route['neighbor']['ip'] != self.ip):
                     if ('update' in route['neighbor']):
                         if ('attribute' in route['neighbor']['update']):
                             attribute = route['neighbor']['update']['attribute']
@@ -36,42 +37,53 @@ class peer():
                             announce = route['neighbor']['update']['announce']
                             if ('ipv4 unicast' in announce):
                                 for prefix in announce['ipv4 unicast'].keys():
-                                    self.rib[prefix] = (announce['ipv4 unicast'][prefix]['next-hop'],origin,','.join(map(str,as_path)),med,atomic_aggregate)
-                                    self.rib.commit()
+                                    self.input_rib[prefix] = (announce['ipv4 unicast'][prefix]['next-hop'],origin,','.join(map(str,as_path)),med,atomic_aggregate)
+                                    self.input_rib.commit()
+                                    bp = self.input_rib.decision_process(prefix)
+                                    if (NULL != bp)
+                                        self.local_rib.del_route(prefix)
+                                        self.local_rib[prefix] = bp
+                                        self.local_rib.commit()
+                                        self.announce_route(bp)
+
                         elif ('withdraw' in route['neighbor']['update']):
                             withdraw = route['neighbor']['update']['withdraw']
                             if ('ipv4 unicast' in withdraw):
                                 for prefix in withdraw['ipv4 unicast'].keys():
-                                    self.rib.delete(prefix)
-                                    self.rib.commit()
+                                    self.input_rib.delete(prefix,route['neighbor']['ip])
+                                    self.input_rib.commit()
+                                    #TODO: need to deal with the local_rib's side of things and reannouncements
+
         elif ('notification' in route):
             if ('shutdown' == route['notification']):
-                self.rib.delete_all
-                self.rib.commit()
+                self.input_rib.delete_all()
+                self.input_rib.commit()
                                     
     def get_route(self,prefix):
         
-        return self.rib.get(prefix)
+        return self.input_rib.get(prefix)
     
     def get_all_routes(self):
         
-        return self.rib.get_all()
+        return self.input_rib.get_all()
     
     def del_route(self,prefix):
         
-        self.rib.delete(prefix)
-        self.rib.commit()
+        self.input_rib.delete(prefix)
+        self.input_rib.commit()
     
     def filter_route(self,item,value):
         
-        return self.rib.filter(item,value)
+        return self.input_rib.filter(item,value)
     
-    def announce_route(self):
+    def announce_route(self,route):
         
         #TODO: add logic for announcing routes
+        #build up route to send
+
         
         pass
-    
+
 ''' main '''    
 if __name__ == '__main__':
     
