548b852d82f02c2d35175065312be5f76df23fb7
Muhammad Shahbaz <muhammad.shahbaz@gatech.edu>
2013-12-28 01:07:59 -0500

    minor corrections for event.set() function placement

--- QuaggaInterface/quagga_interface.py
+++ QuaggaInterface/quagga_interface.py
@@ -104,8 +104,7 @@ def process_json(message,sdx,event):
         else:
             sdx.participant_to_ebgp_nh_received=participant_to_ebgp_nh_received1
             sdx.prefixes_announced=prefixes_announced1
-    
-            event.set()
+        event.set()
             
     # Update the rib with this new BGP Update    
     update_rib(jmesg_new,sdx)    
