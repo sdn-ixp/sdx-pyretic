194bed40d54cc42b3badd6e9b65ce916ca82977e
Muhammad Shahbaz <muhammad.shahbaz@gatech.edu>
2014-01-01 21:45:46 -0500

    Updated RIB to store routes in a persisitent DB and to read on startup. Also, handled thread sysnchornozation issues.

--- bgp/rib.py
+++ bgp/rib.py
@@ -2,48 +2,56 @@
 #  Author:
 #  Muhammad Shahbaz (muhammad.shahbaz@gatech.edu)
 
+import os
 import sqlite3
+from threading import RLock as lock
+
+''' Get current working directory ''' 
+cwd = os.getcwd()
 
 class rib():
     
-    def __init__(self, name):
+    def __init__(self,ip,name):
         
-        # Create a database in RAM
-        self.db = sqlite3.connect(':memory:')
-        self.db.row_factory = sqlite3.Row
-        self.name = name
+        with lock():
+            # Create a database in RAM
+            self.db = sqlite3.connect(cwd+'/pyretic/sdx/ribs/'+ip+'.db',check_same_thread=False)
+            self.db.row_factory = sqlite3.Row
+            self.name = name
         
-        # Get a cursor object
-        cursor = self.db.cursor()
-        cursor.execute('''
-                        create table ''' + self.name + ''' (prefix text, next_hop text,
+            # Get a cursor object
+            cursor = self.db.cursor()
+            cursor.execute('''
+                        create table if not exists '''+self.name+''' (prefix text, next_hop text,
                                origin text, as_path text, med integer, atomic_aggregate boolean)
-        ''')
+            ''')
        
-        self.db.commit()
+            self.db.commit()
     
     def __del__(self):
             
-        self.db.close()
+        with lock():
+            self.db.close()
         
     def __setitem__(self,key,item): 
         
         self.add(key,item)
         
     def __getitem__(self,key): 
-            
+        
         return self.get(key)
         
     def add(self,key,item):
         
-        cursor = self.db.cursor()
+        with lock():
+            cursor = self.db.cursor()
         
-        if (isinstance(item,tuple) or isinstance(item,list)):
-            cursor.execute('''insert into ''' + self.name + ''' (prefix, next_hop, origin, as_path, med,
+            if (isinstance(item,tuple) or isinstance(item,list)):
+                cursor.execute('''insert into ''' + self.name + ''' (prefix, next_hop, origin, as_path, med,
                         atomic_aggregate) values(?,?,?,?,?,?)''', 
                         (key,item[0],item[1],item[2],item[3],item[4]))
-        elif (isinstance(item,dict) or isinstance(item,sqlite3.Row)):
-            cursor.execute('''insert into ''' + self.name + ''' (prefix, next_hop, origin, as_path, med,
+            elif (isinstance(item,dict) or isinstance(item,sqlite3.Row)):
+                cursor.execute('''insert into ''' + self.name + ''' (prefix, next_hop, origin, as_path, med,
                         atomic_aggregate) values(?,?,?,?,?,?)''', 
                         (key,item['next_hop'],item['origin'],item['as_path'],item['med'],
                          item['atomic_aggregate']))
@@ -52,84 +60,94 @@ class rib():
             
     def add_many(self,items):
         
-        cursor = self.db.cursor()
+        with lock():
+            cursor = self.db.cursor()
         
-        if (isinstance(items,list)):
-            cursor.execute('''insert into ''' + self.name + ''' (prefix, next_hop, origin, as_path, med,
+            if (isinstance(items,list)):
+                cursor.execute('''insert into ''' + self.name + ''' (prefix, next_hop, origin, as_path, med,
                         atomic_aggregate) values(?,?,?,?,?,?)''', items)
             
     def get(self,key): 
-            
-        cursor = self.db.cursor()
-        cursor.execute('''select * from ''' + self.name + ''' where prefix = ?''', (key,))
         
-        return cursor.fetchone()
+        with lock():
+            cursor = self.db.cursor()
+            cursor.execute('''select * from ''' + self.name + ''' where prefix = ?''', (key,))
+        
+            return cursor.fetchone()
     
     def get_all(self,key=None): 
-            
-        cursor = self.db.cursor()
         
-        if (key is not None):
-            cursor.execute('''select * from ''' + self.name + ''' where prefix = ?''', (key,))
-        else:
-            cursor.execute('''select * from ''' + self.name)
+        with lock():    
+            cursor = self.db.cursor()
         
-        return cursor.fetchall()
+            if (key is not None):
+                cursor.execute('''select * from ''' + self.name + ''' where prefix = ?''', (key,))
+            else:
+                cursor.execute('''select * from ''' + self.name)
+        
+            return cursor.fetchall()
     
     def filter(self,item,value): 
             
-        cursor = self.db.cursor()
+        with lock():
+            cursor = self.db.cursor()
         
-        script = "select * from " + self.name + " where " + item + " like '%" + value + "%'"
+            script = "select * from " + self.name + " where " + item + " like '%" + value + "%'"
         
-        cursor.execute(script)
+            cursor.execute(script)
         
-        return cursor.fetchall()
+            return cursor.fetchall()
     
     def update(self,key,item,value):
         
-        cursor = self.db.cursor()
+        with lock():
+            cursor = self.db.cursor()
         
-        script = "update " + self.name + " set " + item + " = '" + value + "' where prefix = '" + key + "'"
+            script = "update " + self.name + " set " + item + " = '" + value + "' where prefix = '" + key + "'"
         
-        cursor.execute(script)
+            cursor.execute(script)
             
     def update_many(self,key,item):
         
-        cursor = self.db.cursor()
+        with lock():
+            cursor = self.db.cursor()
         
-        if (isinstance(item,tuple) or isinstance(item,list)):
-            cursor.execute('''update ''' + self.name + ''' set next_hop = ?, origin = ?, as_path = ?,
+            if (isinstance(item,tuple) or isinstance(item,list)):
+                cursor.execute('''update ''' + self.name + ''' set next_hop = ?, origin = ?, as_path = ?,
                             med = ?, atomic_aggregate = ? where prefix = ?''',
                             (item[0],item[1],item[2],item[3],item[4],key))
-        elif (isinstance(item,dict) or isinstance(item,sqlite3.Row)):
-            cursor.execute('''update ''' + self.name + ''' set next_hop = ?, origin = ?, as_path = ?,
+            elif (isinstance(item,dict) or isinstance(item,sqlite3.Row)):
+                cursor.execute('''update ''' + self.name + ''' set next_hop = ?, origin = ?, as_path = ?,
                             med = ?, atomic_aggregate = ? where prefix = ?''', 
                             (item['next_hop'],item['origin'],item['as_path'],item['med'],
                              item['atomic_aggregate'],key))
         
     def delete(self,key):
         
-        # TODO: Add more granularity in the delete process i.e., instead of just prefix, 
-        # it should be based on a conjunction of other attributes too.
+        with lock():
+            # TODO: Add more granularity in the delete process i.e., instead of just prefix, 
+            # it should be based on a conjunction of other attributes too.
         
-        cursor = self.db.cursor()
+            cursor = self.db.cursor()
         
-        cursor.execute('''delete from ''' + self.name + ''' where prefix = ?''', (key,))
+            cursor.execute('''delete from ''' + self.name + ''' where prefix = ?''', (key,))
         
     def delete_all(self):
         
-        cursor = self.db.cursor()
+        with lock():
+            cursor = self.db.cursor()
         
-        cursor.execute('''delete from ''' + self.name)
+            cursor.execute('''delete from ''' + self.name)
     
     def commit(self):
         
-        self.db.commit()
+        with lock():
+            self.db.commit()
         
     def rollback(self):
         
-        self.db.rollback()
+        with lock():
+            self.db.rollback()
 
 ''' main '''     
 if __name__ == '__main__':
