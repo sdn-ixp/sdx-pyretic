67abac510edd878808feab74c1517f6e6bb66252
Muhammad Shahbaz <muhammad.shahbaz@gatech.edu>
2014-01-01 21:42:22 -0500

    added utility functions in the utils module

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,7 @@
 from pyretic.lib.corelib import *
 from pyretic.lib.std import *
 
+''' Get IP to MAC list '''
 def get_ip_mac_list(ip_list,mac_list):
     
     # TODO: why we have this extra value in the header, i.e., VNH, that needs to ignored.
@@ -15,4 +16,17 @@ def get_ip_mac_list(ip_list,mac_list):
         ip_mac_list[IPAddr(ip_list[key])]=EthAddr(mac_list[key])
         
     return ip_mac_list
+
+''' Get participant to ports list '''
+def get_participants_ports_list(participants):
+    participants_list = {}
+    
+    for participant_name in participants:
+        participants_list[participant_name] = []
+                            
+        for port in participants[participant_name].phys_ports:
+            participants_list[participant_name].append(str(port.ip))
+            
+    return participants_list
+        
 
