b704a8c7338c9d86fae621b1eb52714cf3d55843
Muhammad Shahbaz <muhammad.shahbaz@gatech.edu>
2014-01-26 21:29:31 -0500

    minor updates

--- examples/app_specific_peering/participant_D.py
+++ examples/app_specific_peering/participant_D.py
@@ -77,8 +77,8 @@ def policy(participant, sdx):
     #               )
     
     final_policy= (
-                   (match(dstport=80) >> sdx.fwd(participant.peers['A'])) +
-                   ((match(dstport=25) + match(dstport=53)) >> sdx.fwd(participant.peers['B'])) +
+                   ((match(dstport=80) + match(dstport=25)) >> sdx.fwd(participant.peers['A'])) +
+                   #(match(dstport=25) >> sdx.fwd(participant.peers['B'])) +
                    (match_prefixes_set(set(prefixes_announced)) >> sdx.fwd(participant.phys_ports[0]))
                 )
     
