b532d77011e83535e71816b9a4950f7daffad795
castroflavio <castro.flavio@gatech.edu>
2014-12-22 15:31:30 -0500

    Quagga star topology Deployed on Geni
    Rspec 1.6 is working

---
+++ examples/GeniIXP/exabgp/client.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+#  Author:
+#  Muhammad Shahbaz (muhammad.shahbaz@gatech.edu)
+#  Arpit Gupta
+
+import sys
+from threading import Thread
+from multiprocessing.connection import Client
+import os
+home_path = os.environ['HOME']
+logfile = home_path + '/sdx/bgp/client.log'
+sys.path.append(home_path + '/sdx/bgp')
+
+'''Write output to stdout'''
+def _write(stdout,data):
+    stdout.write(data + '\n')
+    stdout.flush()
+
+''' Sender function '''
+def _sender(conn,stdin,log):
+	# Warning: when the parent dies we are seeing continual newlines, so we only access so many before stopping
+	counter = 0
+
+	while True:
+		try:
+			line = stdin.readline().strip()
+	
+			if line == "":
+				counter += 1
+				if counter > 100:
+					break
+				continue
+			counter = 0
+
+			conn.send(line)
+						
+			#log.write(line + '\n')
+			#log.flush()
+		
+		except:
+			pass
+	
+''' Receiver function '''
+def _receiver(conn,stdout,log):
+	
+	while True:
+		try:
+			line = conn.recv()
+	
+			if line == "":
+				continue
+			
+			_write(stdout, line) 
+			''' example: announce route 1.2.3.4 next-hop 5.6.7.8 as-path [ 100 200 ] '''
+			
+			#log.write(line + '\n')
+			#log.flush()
+		
+		except:
+			pass
+
+''' main '''	
+if __name__ == '__main__':
+	
+	log = open(logfile, "w")
+		
+	conn = Client(('143.215.216.192', 6000), authkey='sdx')
+	
+	sender = Thread(target=_sender, args=(conn,sys.stdin,log))
+	sender.start()
+
+	receiver = Thread(target=_receiver, args=(conn,sys.stdout,log))
+	receiver.start()
+	
+	sender.join()
+	receiver.join()
+	
+	log.close()
+
--- examples/GeniIXP/scripts/geni-setup.sh
+++ examples/GeniIXP/scripts/geni-setup.sh
@@ -6,19 +6,38 @@
 # December 2014
 ################################################################################
 BGPD="/etc/quagga/bgpd.conf"
-ROUTER=$1
+
 if [ ! -f $BGPD ]
 then
-    #install quagga, wireshark and nmap
-    sudo apt-get -y install quagga wireshark
-    #Adding permissions to wireshark
-    sudo setcap 'CAP_NET_RAW+eip CAP_NET_ADMIN+eip' /usr/bin/dumpcap
-    cd ~/
-    git clone https://github.com/sdn-ixp/sdx
-    cd sdx
-    git checkout FlavioGeni
-    sudo cp examples/GeniIXP/quaggacfgs/$ROUTER/* /etc/quagga
-    sudo /etc/init.d/quagga restart
+    case "$1" in
+    exabgp)
+       echo "Starting ExaBGP ..."
+       sudo apt-get -y install wireshark
+       sudo setcap 'CAP_NET_RAW+eip CAP_NET_ADMIN+eip' /usr/bin/dumpcap
+       # git clone https://github.com/sdn-ixp/sdx
+       # cd sdx
+       # git checkout FlavioGeni
+       # sudo cp examples/GeniIxp/exabgp/* bgp
+       # $HOME/sdx/exabgp/sbin/exabgp --env $HOME/sdx/exabgp/etc/exabgp/exabgp.env $HOME/sdx/bgp/bgp.conf
+      ;;
+     quagga)
+        ROUTER=$2
+        echo "Configuring Quagga..."
+        #install quagga, wireshark and nmap
+        sudo apt-get -y install quagga wireshark iperf
+        #Adding permissions to wireshark
+        sudo setcap 'CAP_NET_RAW+eip CAP_NET_ADMIN+eip' /usr/bin/dumpcap
+        cd /home/
+        git clone https://github.com/sdn-ixp/sdx
+        cd sdx
+        git checkout FlavioGeni
+        sudo cp examples/GeniIXP/quaggacfgs/$ROUTER/* /etc/quagga
+        sudo /etc/init.d/quagga restart
+      ;;
+    *)
+        echo "Usage: geni-setup {exabgp|quagga <router>}"
+      ;;
+    esac
 else
     echo "False"
 fi
